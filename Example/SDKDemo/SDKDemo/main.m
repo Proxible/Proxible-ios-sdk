31ce70da133b09aeffcdf99952bd95dedb60f46e
Conrad Bartels Daal <conrad@proxible.com>
2014-05-13 11:43:25 +0200

    Added Example App
    Signed-off-by: Conrad Bartels Daal <conrad@proxible.com>

---
+++ Example/SDKDemo/SDKDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDKDemo
+//
+//  Created by Conrad Bartels Daal on 5/13/14.
+//  Copyright (c) 2014 Proxible B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSDAppDelegate class]));
+    }
+}
